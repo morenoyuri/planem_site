ca0b0e8f693cfe6e07a7d8efca348a65222e0355
morenoyuri <moreno-yuri@hotmail.com>
2024-02-28 15:32:52 -0300

    Pagina institucional finalizada

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,12 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'footer',
+	standalone: true,
+	templateUrl: 'footer.component.html',
+	styleUrl: 'footer.component.css'
+})
+export class FooterComponent {
+
+}
